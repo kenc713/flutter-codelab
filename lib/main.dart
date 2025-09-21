51c808f7867ef278df832b131b4dfe36aa44d9c0
kenc713 <kenc71388@gmail.com>
2025-09-21 21:44:42 +0900

    単語およびNextボタンのUI調整

--- lib/main.dart
+++ lib/main.dart
@@ -59,29 +59,92 @@ class MyHomePage extends StatelessWidget {
     // MyHomePage では、watchメソッドを使用してアプリの現在の状態に対する変更を追跡
     var appState = context.watch<MyAppState>();
 
+    // 単語のペア(単語の表示のたびにappState全体を参照するのを避ける)
+    var pair = appState.current;
+
     // どの build メソッドも必ず、ウィジェットか、ウィジェットのネストしたツリー（こちらのほうが一般的）を返却
     // Scaffold: 画面の基本骨組みを提供するウィジェット
     return Scaffold(
 
-      // Column: 
-      // Flutterにおける非常に基本的なレイアウトウィジェット
-      // 任意の数の子を従え、それらを上から下へ一列に配置（デフォルトでは上揃え）
-      body: Column(
-        children: [
 
-          // Text：文字列を画面に表示するための基本ウィジェット
-          Text('A random AWESOME idea:'),
+      body: Center(
 
-          // appStateで定義したデータ（単語ペア）を表示
-          Text(appState.current.asLowerCase),
+        // Column: 
+        // Flutterにおける非常に基本的なレイアウトウィジェット
+        // 任意の数の子を従え、それらを上から下へ一列に配置（デフォルトでは上揃え）
+        // Centerウィジェットでラップすることで、Column自体を左右中央に配置
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'), // Flutterでは、行末にもカンマをつけるのが慣習
+        child: Column(
+        
+          // 縦方向に中央揃え
+          mainAxisAlignment: MainAxisAlignment.center,
+        
+        
+        
+          children: [
+        
+            // Text：文字列を画面に表示するための基本ウィジェット
+            //Text('A random AWESOME idea:'),
+        
+            // appStateで定義したデータ（単語ペア）を表示
+            BigCard(pair: pair),
+
+            // 余白を確保するためのウィジェット
+            SizedBox(height: 10),
+
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'), // Flutterでは、行末にもカンマをつけるのが慣習
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 単語表示用のウィジェット
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final theme = Theme.of(context);
+
+    // styleに書式を格納
+    // theme.textThemeの中のdisplayMediumの書式を指定colorでコピー
+    // displayMediumはnullable（null許容）型である。
+    // 型安全なDartでは、nullableなdisplayMediumにcopyWithが使えないが、
+    // !をつけることで、nullが入らない想定であるとコンパイラに伝えることができ、
+    // copyWithが使えるようになる。
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    // カードウィジェットを返却
+    return Card(
+
+      // 色を指定
+      color: theme.colorScheme.primary,
+
+      // 子にパディングウィジェットを追加
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase, 
+          style: style,
+
+          // 画面読み上げソフト用のラベルを指定
+          semanticsLabel: "${pair.first} ${pair.second}",
           ),
-        ],
       ),
     );
   }
